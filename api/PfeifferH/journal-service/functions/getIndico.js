e26f5f458b85391389db55b21e978315ff5c315f
Zhijian Wang <ew98@foxmail.com>
2019-02-02 18:07:22 -0500

    Upload express js code to connect Indico

---
+++ api/PfeifferH/journal-service/functions/getIndico.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+app.use(express.json())
+const fetch = require("node-fetch");
+var port = 3000;
+
+app.get('/', function(req, res) {
+    
+    var input_data = "";
+    req.on('data', (chunk) => {
+        input_data += chunk;
+        // console.log('data');
+    });
+
+    req.on('end', ()=>{
+        console.log(input_data.text);
+        console.log('======');
+
+        fetch('https://apiv2.indico.io/places', {
+            method: 'POST',
+            body: JSON.stringify({
+                api_key: '3deac03d4a4ed32e154aa18e4742fecf',
+                data: input_data.text
+            })
+        })
+        .then(r => r.json())
+        // TODO: Send a success/failure response to the client
+        .then(response => {
+            // TODO: Store information to firebase
+            console.log(response);
+        })
+        .catch(err => console.log(err));
+    });
+
+    res.send("endget");
+
+});
+
+app.post('/', function (req, res) {
+
+    var input_data = "";
+    req.on('data', (chunk) => {
+        input_data += chunk;
+        // console.log('data');
+    });
+
+    req.on('end', ()=>{
+        //console.log(input_data);
+        //console.log('======');
+        input_data = JSON.parse(input_data);
+        //console.log(input_data);
+        //console.log('======');
+        //console.log(input_data.text);
+        //console.log('======');
+
+        fetch('https://apiv2.indico.io/emotion', {
+            method: 'POST',
+            body: JSON.stringify({
+                api_key: '3deac03d4a4ed32e154aa18e4742fecf',
+                data: input_data.text
+            })
+        })
+        .then(r => r.json())
+        // TODO: Send a success/failure response to the client
+        .then(response => {
+            // TODO: Store information to firebase
+            console.log(response);
+        })
+        .catch(err => console.log(err));
+    });
+
+    res.send("endpost");
+
+});
+
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
