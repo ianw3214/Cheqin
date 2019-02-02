ced469dd4adccbc5f7fccd25bcb70941eb555a9e
Ian <ianw3214@gmail.com>
2019-02-02 14:30:55 -0500

    Minor changes to the width

--- app/src/homePage.js
+++ app/src/homePage.js
@@ -4,7 +4,7 @@ import LoginPage from "./loginPage";
 class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div id="main_container">
         <h2>{this.props.appName}</h2>
         <h4>Journal and reflect on your day using the Google Assistant.</h4>
         <p>Welcome. Life is full of emotional ups and downs, we want to help you realize what makes you happy. Your voice journal entries are transcribed, analyzed anonymously using Machine Learning algorithms, securely stored and displayed to you in your personal online journal.</p>
