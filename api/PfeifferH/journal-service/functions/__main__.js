73021bd38e441a8d380aa41ccf5312e05472a165
PfeifferH <15hp19@queensu.ca>
2019-02-02 04:08:39 -0500

    Added login credentials to initialize firebase

--- api/PfeifferH/journal-service/functions/__main__.js
+++ api/PfeifferH/journal-service/functions/__main__.js
@@ -1,8 +1,17 @@
+
 /**
 * A basic Hello World function
 * @param {string} name Who you're saying hello to
 * @returns {string}
 */
+let admin = require('firebase-admin')
+
+let serviceAccount = require('../../../../settings.json')
+
 module.exports = async (name = 'world', context) => {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://journalagent-db480.firebaseio.com"
+  })
   return `hello ${name}`;
 };
