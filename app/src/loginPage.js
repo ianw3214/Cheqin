3f5b0abe1ff1360628093494a20a51b6e503eb3c
Angelo Lu <angelo.lu@hotmail.ca>
2019-02-03 05:57:08 -0500

    Implemented Auth.0

--- app/src/loginPage.js
+++ app/src/loginPage.js
@@ -4,12 +4,15 @@ import {Redirect} from "react-router-dom";
 class LoginPage extends Component {
   
     render() {
+      //this.props.auth.login();
       if (!this.props.isSignedIn) {
+        
         return (
           <div>
             <h2>Sign in to {this.props.appName}</h2>
             <h4>Journal and reflect on your day using the Google Assistant.</h4>
             <p>Encourage more self reflection, promote the expression of emotions and monitor mental wellbeing over time</p>
+            <button type="button" onClick={() => this.props.auth.login()} class="btn btn-primary">Log In</button>
           </div>
         );
       }
---
+++ app/src/routes.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Route, Router, NavLink,Redirect } from 'react-router-dom';
+import App from './App';
+import Callback from './callback/Callback.js';
+import Auth from './Auth/Auth.js';
+import history from './history';
+import HomePage from "./homePage";
+import firebase from "firebase";
+import LoginPage from "./loginPage";
+import JournalEntries from "./journalEntries";
+
+  
+  // Configure Firebase.
+  var config = {
+    apiKey: "AIzaSyDnmprTKJt-VYkhVDryq9G4a_2JjHMYVJM",
+    authDomain: "daily-cheqin.firebaseapp.com",
+    databaseURL: "https://daily-cheqin.firebaseio.com",
+    projectId: "daily-cheqin",
+    storageBucket: "daily-cheqin.appspot.com",
+    messagingSenderId: "709508738073"
+  };
+  firebase.initializeApp(config);
+  
+  const appName = "Daily CheqIn";
+  const auth = new Auth();
+  
+
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+      <Router history={history}>
+      
+        <div>
+        <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} /> 
+          }}/>
+          <Route path="/" render={(props) => <App appName={appName} auth={auth} innerContents={<>
+          <Route exact path="/" render={(routeProps) => (<HomePage appName={appName}  auth={auth} />)}/>
+              <Route path="/journal" render={(routeProps) => (<JournalEntries auth={auth} appName={appName}  />)}/>
+              <Route path="/login" render={(routeProps) => (<LoginPage auth={auth} appName={appName} />)}/></>} />} />
+          
+          
+          
+        </div>
+      </Router>
+  );
+}
