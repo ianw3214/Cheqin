575b32393afb3664633ac3ef4a574a4663cc924f
PfeifferH <15hp19@queensu.ca>
2019-02-03 08:27:55 -0500

    Fixed minor bugs in chart displaying

--- app/src/weekoverview.js
+++ app/src/weekoverview.js
@@ -6,31 +6,39 @@ class WeekOverview extends Component {
   constructor(props) {
     super(props);
     this.state = {entries:[]}
-    fetch("https://pfeifferh.lib.id/journal-service@dev/getSessions/?userId=FFYOkVqrfebqih4m6ZyI")
-        .then(response => {
-            response.text().then((text) => {
-                let data = JSON.parse(text);
-                
-                this.setState({entries:data});
-                // do something with the text response 
-              });
-        });
+    
   }
-  render() {   
+
+  async componentDidMount() {
+    await fetch("https://pfeifferh.lib.id/journal-service@dev/getSessions/?userId=FFYOkVqrfebqih4m6ZyI")
+      .then(response => {
+        response.text().then((text) => {
+          let data = JSON.parse(text);
+          
+          this.setState({entries:data});
+        // do something with the text response 
+        });
+      });
+  }
+
+  render() {
+    if(!this.state.entries) return null   
     let emotionVals = [0, 0, 0, 0, 0]
     this.state.entries.forEach(entry => {
-      let emotions = Object.keys(entry[1].emotions)
-      for(let i in emotionVals) {
-        emotionVals[i] += emotions[i]
+      if(entry[1].emotions){
+        console.log(entry)
+        let emotions = Object.values(entry[1].emotions)
+        console.log(emotions)
+        for(let i in emotionVals) {
+          emotionVals[i] += emotions[i]
+        }
+        console.log(emotionVals)
       }
     }) 
-    let averageVals = [0, 0, 0, 0, 0];
-    
     emotionVals.forEach(emotion => {
-      averageVals = [averageVals[0] + emotionVals[0], averageVals[1] + emotionVals[1], averageVals[2] + emotionVals[2], averageVals[3] + emotionVals[3], averageVals[4] + emotionVals[4], averageVals[5]+ emotionVals[5]];
+      
+      emotion = emotion / this.state.entries.length 
     });
-    let totalsum = averageVals[0] + averageVals[1] + averageVals[2] + averageVals[3] + averageVals[4] + averageVals[5] + averageVals[6];
-    averageVals = [averageVals[0] / totalsum, averageVals[1] / totalsum, averageVals[2] / totalsum, averageVals[3] / totalsum, averageVals[4]/ totalsum, averageVals[5]/ totalsum];
     
     
     let data = {
