7e55b24eb0dbbb78d6050887e7ac150ede1b97d3
Angelo Lu <angelo.lu@hotmail.ca>
2019-02-03 06:28:08 -0500

    Small fix

--- app/src/loginPage.js
+++ app/src/loginPage.js
@@ -12,7 +12,7 @@ class LoginPage extends Component {
             <h2>Sign in to {this.props.appName}</h2>
             <h4>Journal and reflect on your day using the Google Assistant.</h4>
             <p>Encourage more self reflection, promote the expression of emotions and monitor mental wellbeing over time</p>
-            <button type="button" onClick={() => this.props.auth.login()} class="btn btn-primary">Log In</button>
+            <button type="button" onClick={() => this.props.auth.login()} className="btn btn-primary">Log In</button>
           </div>
         );
       }
