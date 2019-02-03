22def5508ca3eef39a39f5612d71a170e0aa87fb
Angelo Lu <angelo.lu@hotmail.ca>
2019-02-02 21:16:16 -0500

    JOVO NEEDS TO WORK

--- jovo/Journal/src/app.js
+++ jovo/Journal/src/app.js
@@ -4,13 +4,27 @@
 // APP INITIALIZATION
 // ------------------------------------------------------------------
 
-const { App } = require('jovo-framework');
-const { Alexa } = require('jovo-platform-alexa');
-const { GoogleAssistant } = require('jovo-platform-googleassistant');
-const { JovoDebugger } = require('jovo-plugin-debugger');
-const { FileDb } = require('jovo-db-filedb');
+const {
+    App
+} = require('jovo-framework');
+const {
+    Alexa
+} = require('jovo-platform-alexa');
+const {
+    GoogleAssistant
+} = require('jovo-platform-googleassistant');
+const {
+    JovoDebugger
+} = require('jovo-plugin-debugger');
+const {
+    FileDb
+} = require('jovo-db-filedb');
 
 const app = new App();
+
+const firebase_tools = require('firebase-tools');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 app.use(
     new Alexa(),
@@ -26,20 +40,23 @@ app.use(
 
 app.setHandler({
     LAUNCH() {
+        this.$data.transcript = "";
+        this.$data.summary = "";
         return this.toIntent('InitialIntent');
     },
 
     InitialIntent() {
         this.followUpState('JournalLogState').
-            ask('Welcome back, how was your day?', 'Please tell me about your day.');
+        ask('Welcome back, how was your day?', 'Please tell me about your day.');
     },
 
     JournalLogState: {
         JournalDoneIntent() {
             this.followUpState('EmotionLogState').
-                ask('Overall, how would you summarize how you felt in one word?', "Please give me a one word summary of your day");
+            ask('Overall, how would you summarize how you felt in one word?', "Please give me a one word summary of your day");
         },
         JournalLogIntent() {
+            this.$data.transcript = this.$data.transcript + this.$inputs.log.value;
             this.followUpState('JournalLogState')
                 .ask('Did you do anything else?\n' + this.$inputs.log.value, 'What else did you do today?');
         }
@@ -47,8 +64,38 @@ app.setHandler({
 
     EmotionLogState: {
         EmotionLogIntent() {
+            this.$data.summary = this.$inputs.emotion.value;
             this.tell('Alright, got it. Thanks for sharing!');
+
+            var uid = "";
+            console.log("WHAT");
+            return admin.auth().verifyIdToken(this.$user.idToken)
+                .then((decodedToken) => {
+                    console.log("Token decoded");
+                    uid = decodedToken.uid;
+                    console.log("Decode: " + uid);
+                    return admin.firestore().collection('users/' + uid + '/sessions').add({
+                        tokenRefreshTime: FieldValue.serverTimestamp(),
+                        date: this.getTimestamp,
+                        text: this.$data.transcript,
+                        summary: this.$data.summary
+                    }).then((document) => {
+                        console.log("Document saved at " + 'users/' + uid + '/sessions' + document.id);
+                        return 0;
+                    }).catch((e) =>{
+                        return e;
+                    });
+                    // ...
+                }).catch((error) => {
+                    // Handle error
+                    console.log(error);
+                    return error;
+                });
+                
+
+
             // this.$inputs.emotion.value <- FOR THE INPUT
+            
         }
     }
 
