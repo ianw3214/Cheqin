feda259088e68ded3ab206a093bd6b955e7a26e9
PfeifferH <15hp19@queensu.ca>
2019-02-03 07:03:19 -0500

    updated database url

--- api/PfeifferH/journal-service/functions/getSessions.js
+++ api/PfeifferH/journal-service/functions/getSessions.js
@@ -12,7 +12,7 @@ let serviceAccount = {
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://journalagent-db480.firebaseio.com'
+  databaseURL: "https://daily-cheqin.firebaseio.com"
 })
 
 module.exports = async (userToken='', userId=null, context) => {
