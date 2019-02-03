266c0d9cc010493e09b31d202fee0b01a4365354
PfeifferH <15hp19@queensu.ca>
2019-02-03 01:41:31 -0500

    add react-chartjs

--- app/src/App.js
+++ app/src/App.js
@@ -11,7 +11,7 @@ import {
   import LoginPage from "./loginPage";
   import JournalEntries from "./journalEntries";
 
-  import firebase from "firebase";
+  // import firebase from "firebase";
   
   // Configure Firebase.
 const config = {
@@ -22,107 +22,108 @@ const config = {
     storageBucket: "journalagent-db480.appspot.com",
     messagingSenderId: "472886687367"
 };
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
-  const appName = "Daily CheqIn";
+const appName = "Daily CheqIn";
+
 class App extends Component {
 
 
     // Listen to the Firebase Auth state and set the local state.
-    componentDidMount() {
-      this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-          (user) => {
-            this.onAuthStateChanged(firebase.auth().currentUser);
-            this.setState({isSignedIn: !!user, isLoaded: true})
-          }
-      );
-    }
+    // componentDidMount() {
+    //   this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+    //       (user) => {
+    //         this.onAuthStateChanged(firebase.auth().currentUser);
+    //         this.setState({isSignedIn: !!user, isLoaded: true})
+    //       }
+    //   );
+    // }
     
     // Make sure we un-register Firebase observers when the component unmounts.
-    componentWillUnmount() {
-      this.unregisterAuthObserver();
-    }
+    // componentWillUnmount() {
+    //   this.unregisterAuthObserver();
+    // }
       
   constructor(props) {
     super(props);
-    this.onAuthStateChanged = this.onAuthStateChanged.bind(this);
+    // this.onAuthStateChanged = this.onAuthStateChanged.bind(this);
     this.state = {isLoaded: false, isSignedIn: false, authUser: {}, userName: "Not signed in", userPhoto: null}
   }
 
-  onAuthStateChanged(authUser){
-    this.setState({ authUser: authUser})
-    if(authUser != null) {
-      this.setState({ userName: authUser.displayName, userPhoto: authUser.photoURL})
-    }else{
-      this.setState({ userName: "Not signed in", userPhoto: null})
-    }
-  }
+  // onAuthStateChanged(authUser){
+  //   this.setState({ authUser: authUser})
+  //   if(authUser != null) {
+  //     this.setState({ userName: authUser.displayName, userPhoto: authUser.photoURL})
+  //   }else{
+  //     this.setState({ userName: "Not signed in", userPhoto: null})
+  //   }
+  // }
 
   render() {
     if(this.state.isLoaded){
       return (
         <HashRouter>
           <div className="container-scroller App">
-      <nav className="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-        <div className="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-          {/*<a className="navbar-brand brand-logo" href="index.html">
-            <img src="images/logo.svg" alt="logo" />
-          </a>
-          <a className="navbar-brand brand-logo-mini" href="index.html">
-            <img src="images/logo-mini.svg" alt="logo" />
-          </a>*/}
-        </div>
-        <div className="navbar-menu-wrapper d-flex align-items-center">
-          <ul className="navbar-nav navbar-nav-right">
-          {this.state.isSignedIn ? <>
-            <li className="nav-item dropdown">
-              <a className="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-                <i className="mdi mdi-bell"></i>
-                <span className="count">1</span>
-              </a>
-              <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-                <a className="dropdown-item">
-                  <p className="mb-0 font-weight-normal float-left">You have 1 new notifications
-                  </p>
-                  <span className="badge badge-pill badge-warning float-right">View all</span>
+            <nav className="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+            <div className="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+            {/*<a className="navbar-brand brand-logo" href="index.html">
+              <img src="images/logo.svg" alt="logo" />
+            </a>
+            <a className="navbar-brand brand-logo-mini" href="index.html">
+              <img src="images/logo-mini.svg" alt="logo" />
+            </a>*/}
+          </div>
+          <div className="navbar-menu-wrapper d-flex align-items-center">
+            <ul className="navbar-nav navbar-nav-right">
+            {this.state.isSignedIn ? <>
+              <li className="nav-item dropdown">
+                <a className="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
+                  <i className="mdi mdi-bell"></i>
+                  <span className="count">1</span>
                 </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item preview-item">
-                  <div className="preview-thumbnail">
-                    <div className="preview-icon bg-success">
-                      <i className="mdi mdi-alert-circle-outline mx-0"></i>
-                    </div>
-                  </div>
-                  <div className="preview-item-content">
-                    <h6 className="preview-subject font-weight-medium text-dark">Welcome to Daily Cheqin</h6>
-                    <p className="font-weight-light small-text">
-                      Just now
+                <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                  <a className="dropdown-item">
+                    <p className="mb-0 font-weight-normal float-left">You have 1 new notifications
                     </p>
-                  </div>
+                    <span className="badge badge-pill badge-warning float-right">View all</span>
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item preview-item">
+                    <div className="preview-thumbnail">
+                      <div className="preview-icon bg-success">
+                        <i className="mdi mdi-alert-circle-outline mx-0"></i>
+                      </div>
+                    </div>
+                    <div className="preview-item-content">
+                      <h6 className="preview-subject font-weight-medium text-dark">Welcome to Daily Cheqin</h6>
+                      <p className="font-weight-light small-text">
+                        Just now
+                      </p>
+                    </div>
+                  </a>
+                  <div className="dropdown-divider"></div>
+                </div>
+              </li>
+              <li className="nav-item dropdown d-none d-xl-inline-block">
+                <a className="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+                  
+                  <span className="profile-text">Hello, {this.state.userName}!</span><img className="img-xs rounded-circle" src={this.state.userPhoto} alt="Profile"></img>
+                  
                 </a>
-                <div className="dropdown-divider"></div>
-              </div>
-            </li>
-            <li className="nav-item dropdown d-none d-xl-inline-block">
-              <a className="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                
-                <span className="profile-text">Hello, {this.state.userName}!</span><img className="img-xs rounded-circle" src={this.state.userPhoto} alt="Profile"></img>
-                
-              </a>
-              <div className="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
-                <a className="dropdown-item mt-2">
-                  Account Settings
-                </a>
-                <a className="dropdown-item" onClick={() => firebase.auth().signOut()}>
-                  Sign Out
-                </a>
-              </div>
-            </li></> : ''}
-          </ul>
-          <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-            <span className="mdi mdi-menu"></span>
-          </button>
-        </div>
+                <div className="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
+                  <a className="dropdown-item mt-2">
+                    Account Settings
+                  </a>
+                  {/* <a className="dropdown-item" onClick={() => firebase.auth().signOut()}>
+                    Sign Out
+                  </a> */}
+                </div>
+              </li></> : ''}
+            </ul>
+            <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+              <span className="mdi mdi-menu"></span>
+            </button>
+          </div>
       </nav>
       <div className="container-fluid page-body-wrapper">
         <nav className="sidebar sidebar-offcanvas" id="sidebar">
@@ -141,10 +142,10 @@ class App extends Component {
               </NavLink>
             </li>
             <li className="nav-item">
-            <a className="nav-link" onClick={() => firebase.auth().signOut()}>
+            {/* <a className="nav-link" onClick={() => firebase.auth().signOut()}>
                 <i className="menu-icon mdi mdi-logout"></i>
                 <span className="menu-title">Log Out</span>
-              </a>
+              </a> */}
             </li>
             </> : ''}
 
